375c5670a257de7133bf4f2a4d2d7e0238a962e3
squanchymnonm <monaldi.nicolas@gmail.com>
2026-06-27 04:39:59 +0000

    feat(habitat): setup Tailwind v4 + tokens forja + fuentes self-hosted

--- habitat/client/src/main.ts
+++ habitat/client/src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import './style.css'
+import './styles/theme.css'
 
 createApp(App).use(createPinia()).mount('#app')
